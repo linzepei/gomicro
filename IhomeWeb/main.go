e1bf24ffc238f3d2f0496838123d6e2c76713651
SherLzp <445965239@qq.com>
2019-02-16 20:42:22 +0800

    1.基本框架搭建 2.数据库初始化 3.页面初始化

--- IhomeWeb/main.go
+++ IhomeWeb/main.go
@@ -1,33 +1,41 @@
 package main
 
 import (
-        "github.com/micro/go-log"
+	"github.com/julienschmidt/httprouter"
+	"github.com/micro/go-log"
 	"net/http"
 
-        "github.com/micro/go-web"
-        "gomicro/IhomeWeb/handler"
+	"github.com/micro/go-web"
+	"gomicro/IhomeWeb/handler"
+	_ "gomicro/IhomeWeb/models"
 )
 
 func main() {
 	// create new web service
-        service := web.NewService(
-                web.Name("go.micro.web.IhomeWeb"),
-                web.Version("latest"),
-        )
+	service := web.NewService(
+		web.Name("go.micro.web.IhomeWeb"),
+		web.Version("latest"),
+		web.Address(":8888"),
+	)
+
+	//使用路由中间件来映射页面
+	rou := httprouter.New()
+	rou.NotFound = http.FileServer(http.Dir("html"))
 
 	// initialise service
-        if err := service.Init(); err != nil {
-                log.Fatal(err)
-        }
+	if err := service.Init(); err != nil {
+		log.Fatal(err)
+	}
 
 	// register html handler
-	service.Handle("/", http.FileServer(http.Dir("html")))
+	//service.Handle("/", http.FileServer(http.Dir("html")))
+	service.Handle("/", rou)
 
 	// register call handler
 	service.HandleFunc("/example/call", handler.ExampleCall)
 
 	// run service
-        if err := service.Run(); err != nil {
-                log.Fatal(err)
-        }
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
